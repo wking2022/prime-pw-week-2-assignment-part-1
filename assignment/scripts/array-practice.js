f45f091c4f374faa88462883f988ad0093f62386
Liam King <techy.willyk3@gmail.com>
2022-11-14 23:12:18 -0600

    Went through to review and made several new updates with my new understanding.

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -1,27 +1,21 @@
 console.log('****** Array Practice *******');
 
-// 1. Creating arrays
-console.log('--- 1. Practice creating arrays ---');
+// // 1. Creating arrays
+// console.log('--- 1. Practice creating arrays ---');
 
-// let listOfCookies = ['Chocolate Chip', 'Raisin', 'Sugar', 'M&M'];
-// console.log(listOfCookies);
 
-// console.log( ' The list of cookies Mother will make are:', [listOfCookies]);
-// // Mother plan on making all of my favorites, except she doesnt have chocolate chips, so i have to choose another another to replace Chocolate Chip.
-// listOfCookies.shift();
+// function itsOnn (){
+//     let you = 10;
+//     for (let i=0; i<you; i++){
+//         return 'hello World', you;
+//     }
+// }
 
-// listOfCookies.unshift( 'Peanut Butter' );
-// console.log(listOfCookies);
-// // Mother realized that she actually does have Chocolate Chips, and it was raisins that she didn't have any of.
+// console.log('You will be getting:',itsOnn(), 'punches.');
 
-// listOfCookies[1] = ' Chocolate Chip ';
-// console.log(listOfCookies);
 
-// console.log(listOfCookies.indexOf('Sugar'));
-// // Mother is feeling a bit ambitious today, so she tells me to pick another cookie I'd like also.
+// __________WHOLE NEW SECTION __________
 
-// listOfCookies.push( 'toffee' );
-// console.log(listOfCookies);
 
 
 
@@ -37,7 +31,7 @@ console.log(listOfCookies);
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
 
-console.log( 'Similar to the example above, is',listOfCookies);
+console.log( 'Similar to the example above, is:',listOfCookies);
 
 // 2. Array.length - tells you how many items are in the array
 console.log('--- 2. Length of an array ---');
@@ -53,21 +47,20 @@ console.log( 'Number of cookies:', listOfCookies.length);
 console.log('--- 3. Accessing items in an array ---');
 
 // Example: Log the first animal from the array using it's array index
-console.log('First animal is', animalArray[0])
+console.log('First animal is:', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array 
 
-console.log( 'The second animal in the array is', animalArray[1]);
+console.log( 'The second animal in the array is:', animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
 
-console.log( 'The last animal in the array is', animalArray[3]);
+console.log( 'The last animal in the array is:', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
 
-let lastAnimal = 'Dog'
-console.log( 'Last animal in array is:', lastAnimal.length);
+console.log( 'Last animal in array is:', animalArray[animalArray.length -1]);
 // console.log('Last animal in array is:', animalArray.length)
 
 // 4. Adding & Removing Array Items
@@ -83,8 +76,7 @@ listOfCookies.push( 'Peanut Butter' );
 console.log(listOfCookies);
 
 // Example: Remove the last animal by using Array.pop
-let removedAnimal = animalArray.pop();
-console.log('Removed the last animal', removedAnimal);
+animalArray.pop();
 console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array & 
@@ -97,7 +89,7 @@ console.log(listOfCookies);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
-console.log(`Added an animal to beginning: ${animalArray}`);
+console.log('Added an animal to beginning:', animalArray);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
 
@@ -105,15 +97,14 @@ listOfCookies.unshift( 'toffee');
 console.log(listOfCookies);
 
 // Example: Remove the first animal using Array.shift
-removedAnimal = animalArray.shift();
-console.log('Removed the first animal', removedAnimal);
-console.log('The animals are now', animalArray);
+animalArray.shift();
+console.log('After removing the first animal, the animals are now:', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array & 
 //     log both the food removed and the updated array
 
 removedFoodItem = listOfCookies.shift();
-console.log( 'removed the first food item:', removedFoodItem);
+console.log( 'This is the food item in the begining of the array that was removed:', removedFoodItem);
 console.log(listOfCookies);
 
 // 4.e (STRETCH) TODO: Replace the second food in your array
@@ -139,10 +130,6 @@ console.log(listOfCookies);
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
 
-listOfCookies[0] = 'Sugar'
-listOfCookies[1] = 'Mint'
-listOfCookies[2] = "M&M"
-listOfCookies[3] = "Chocolate Chip"
 
 console.log( listOfCookies[0], 'and' , listOfCookies[1], 'and' , listOfCookies[2], 'and' , listOfCookies[3]);
 
